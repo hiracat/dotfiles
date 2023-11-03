34a99c2ff06a2d90e14bfe3c3c24539280ce84bc
forest lehrman <forestgithub@skiff.com>
2023-11-03 11:24:42 -0400

    added nvim config

---
+++ nvim/after/plugin/dap.lua
@@ -0,0 +1,46 @@
+require("dapui").setup()
+local dap = require('dap')
+dap.adapters.cppdbg = {
+    id = 'cppdbg',
+    type = 'executable',
+    command = '/home/forest/.local/share/nvim/mason/bin/OpenDebugAD7',
+}
+dap.configurations.cpp = {
+    {
+        name = "Launch file",
+        type = "cppdbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopAtEntry = true,
+        setupCommands = {
+            {
+                text = '-enable-pretty-printing',
+                description = 'enable pretty printing',
+                ignoreFailures = false
+            },
+        },
+    },
+    {
+        name = 'Attach to gdbserver :1234',
+        type = 'cppdbg',
+        request = 'launch',
+        MIMode = 'gdb',
+        miDebuggerServerAddress = 'localhost:1234',
+        miDebuggerPath = '/usr/bin/gdb',
+        cwd = '${workspaceFolder}',
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        setupCommands = {
+            {
+                text = '-enable-pretty-printing',
+                description = 'enable pretty printing',
+                ignoreFailures = false
+            },
+        },
+    },
+}
+dap.configurations.c = dap.configurations.cpp
---
+++ nvim/lua/forest/init.lua
@@ -0,0 +1,3 @@
+require("forest.lazy")
+require("forest.set")
+require("forest.remap")
