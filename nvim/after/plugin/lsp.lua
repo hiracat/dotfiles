b2a28fd1540989e4d8f26dcf2d98bb06f06bdfc1
forest lehrman <forestgithub@skiff.com>
2023-11-11 15:18:07 -0500

    fixed language servers

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,21 +1,17 @@
-local lsp = require("lsp-zero").preset({})
+local lsp_zero = require("lsp-zero")
 
-lsp.on_attach(function(client, bufnr)
-	lsp.default_keymaps({ buffer = bufnr })
+lsp_zero.on_attach(function(client, bufnr)
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
--- (Optional) Configure lua language server for neovim
-require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
-
-lsp.setup()
-
-local cmp = require("cmp")
-local cmp_action = require("lsp-zero").cmp_action()
-
-cmp.setup({
-	mapping = {
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		["<Tab>"] = cmp_action.luasnip_supertab(),
-		["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	-- Replace the language servers listed here
+	-- with the ones you want to install
+	ensure_installed = { "clangd", "bashls", "cmake", "lua_ls"},
+	handlers = {
+		lsp_zero.default_setup,
 	},
 })
